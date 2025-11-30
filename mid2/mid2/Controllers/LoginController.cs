3817e47e580f820e1cb0b686f8d9a80757ea322b
Sawhardo Biswas Sikto <22-47406-2@student.aiub.edu>
2025-11-30 20:10:26 +0600

    login reg and crud

--- mid2/mid2/Controllers/LoginController.cs
+++ mid2/mid2/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using mid2.Models;
+﻿using mid2.EF;
+using mid2.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace mid2.Controllers
 {
     public class LoginController : Controller
     {
+        ThirdLabEntities db = new ThirdLabEntities();
         // GET: Login
         public ActionResult Index()
         {
@@ -20,23 +22,38 @@ namespace mid2.Controllers
         public ActionResult Login()
         {
             
-            return View();
+            return View(new Login());
+
         }
         [HttpPost]
         public ActionResult Login(Login login)
         {
             if (ModelState.IsValid)
             {
+                // Database theke email & password match
+                var user = db.Users
+                             .FirstOrDefault(u => u.StudentId == login.Id
+                                               && u.Password == login.Password);
 
-                TempData["msg"] = "Login Successful";
-                return RedirectToAction("Index", "Home");
+                if (user != null)
+                {
+                    TempData["msg"] = "Login Successful";
+                    return RedirectToAction("Index", "Product");
+                }
+                else
+                {
+                    ViewBag.Error = "Invalid Email or Password";
+                }
             }
-            return View();
-        }
-                
 
-            
-        
+            return View(login);
+        }
+
+
+
+
+
+
 
 
         [HttpGet]
@@ -50,6 +67,17 @@ namespace mid2.Controllers
         {
             if(ModelState.IsValid)
             {
+                User u = new User();
+                u.Name = r.Name;
+                u.Password = r.Password;
+                u.StudentId = r.Id;
+                u.Email = r.Email;
+                u.Profession = r.Profession;
+                u.Gender = r.Gender;
+                u.Hobbies = string.Join(",", r.Hobbies);
+                db.Users.Add(u);
+                db.SaveChanges();
+                
                 TempData["msg"] = "Registration Successful";
                 return RedirectToAction("Login");
             }
---
+++ mid2/mid2/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿using mid2.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mid2.Controllers
+{
+    public class ProductController : Controller
+    {
+        ThirdLabEntities db = new ThirdLabEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var data = db.Catagories.ToList();
+            ViewBag.Catagories = data; 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product p)
+
+        {
+            if(ModelState.IsValid)
+            {
+                db.Products.Add(p);
+                db.SaveChanges();
+                TempData["msg"] = "Product Created Successfully";
+                return RedirectToAction("List");
+            }
+            return View(p);
+        }
+        public ActionResult List()
+        {
+            var products = db.Products.ToList();
+            return View(products);
+        }
+        public ActionResult Details(Product p)
+        {
+            var product = db.Products.Find(p.Id);
+            return View(product);
+        }
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            ViewBag.Catagories = db.Catagories.ToList();
+            var product = db.Products.Find(id);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Update(Product p)
+        {
+            var obj = db.Products.Find(p.Id);
+            
+            // db.Entry(obj).CurrentValues.SetValues(p);  ei ek line diyeo update kora jay
+
+            obj.Name = p.Name;
+            obj.Price = p.Price;
+            obj.CId = p.CId;
+            obj.QTY = p.QTY;
+            db.SaveChanges();
+            TempData["msg"] = "Product Updated Successfully";
+            return RedirectToAction("List");
+
+
+
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var product = db.Products.Find(id);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Delete(Product p)
+        {
+            var product = db.Products.Find(p.Id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            TempData["msg"] = "Product Deleted Successfully";
+            return RedirectToAction("List");
+        }
+    }
+
+}
