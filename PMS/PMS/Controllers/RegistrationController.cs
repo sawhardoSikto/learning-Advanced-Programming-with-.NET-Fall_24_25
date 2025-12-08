ad940f43f3816f91d48a8e3e8134a1910229764d
Sawhardo Biswas Sikto <22-47406-2@student.aiub.edu>
2025-12-09 02:09:23 +0600

    PMS added

---
+++ PMS/PMS/Controllers/RegistrationController.cs
@@ -0,0 +1,80 @@
+﻿using PMS.DTOs;
+using PMS.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PMS.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        PMSEntities db = new PMSEntities();
+
+        public static Customer Convert(CustomerDTO c)
+        {
+            return new Customer
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Email = c.Email,
+                Username = c.Username,
+                Password = c.Password
+            };
+        }
+        public static CustomerDTO Convert(Customer c)
+        {
+            return new CustomerDTO
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Email = c.Email,
+                Username = c.Username,
+                Password = c.Password
+            };
+        }
+
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View(new CustomerDTO());
+        }
+        [HttpPost]
+        public ActionResult Register(CustomerDTO c)
+        {
+            if (ModelState.IsValid)
+            {
+                var customer = Convert(c);
+                customer.Password = CreateMD5(c.Password);
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View(c);
+        }
+
+        public static string CreateMD5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2")); // "x2" ensures lowercase hex format
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ PMS/PMS/CustomValidation/ValidUserName.cs
@@ -0,0 +1,34 @@
+﻿using PMS.EF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PMS.CustomValidation
+{
+    public class ValidUserName : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var uname = value.ToString();
+                var db = new PMSEntities();
+                var user = (from u in db.Customers
+                            where u.Username.Equals(uname)
+                            select u).SingleOrDefault(); 
+                if (user == null)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                                       return new ValidationResult("Username already exists");
+                }
+            }
+            return new ValidationResult("Username is required");
+
+        }
+    }
+}
