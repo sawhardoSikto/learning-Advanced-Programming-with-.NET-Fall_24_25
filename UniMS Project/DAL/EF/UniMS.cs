469047febbfdbc063d6d52a6a5a91f91f4ceca19
Sawhardo Biswas Sikto <22-47406-2@student.aiub.edu>
2026-01-09 14:03:12 +0600

    UniMS project start

---
+++ UniMS Project/DAL/EF/UniMS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.EF
+{
+    public class UniMS : DbContext
+    {
+        public UniMS(DbContextOptions<UniMS> options) : base(options)
+        {
+
+        }
+
+    }
+}
